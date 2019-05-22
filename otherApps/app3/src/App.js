7dbb9cd6ed3a487e3504004cdeacbbb5edf59d1b
serikjensen <sjense90@gmail.com>
2019-05-22 16:43:02 -0500

    Making a change

--- otherApps/app3/src/App.js
+++ otherApps/app3/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Some changes to app3 via codesandbox
+          A change
         </a>
       </header>
     </div>
