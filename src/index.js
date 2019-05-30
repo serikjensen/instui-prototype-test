499d95d5b5eaccd92b60086d842e02e6f9ee955c
Stephen Jensen <sejensen@instructure.com>
2019-05-30 16:52:31 -0500

    Introduce react and append it to the root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-/* eslint-disable notice/notice */
-/* eslint-disable no-console */
-console.log('hello foo!')
-/* eslint-enable no-console */
-/* eslint-enable notice/notice */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
