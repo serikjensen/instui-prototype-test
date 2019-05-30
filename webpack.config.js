83e69902691f1b21e2e2fe0f3975fcd48ffae119
Stephen Jensen <sejensen@instructure.com>
2019-05-30 15:53:53 -0500

    Creation of basic prototype app

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const baseConfig = require('@instructure/ui-webpack-config')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const outputPath = path.resolve(__dirname, '__build__')
+
+module.exports = {
+  ...baseConfig,
+  entry: './src/index.js',
+  output: {
+    path: outputPath,
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: outputPath,
+    host: '0.0.0.0'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
